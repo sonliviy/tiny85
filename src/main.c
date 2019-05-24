479569ddb505079f285750c7b42d0443ae386c0a
sonliviy <everydayitank@yandex.ru>
2019-05-24 13:22:22 +0700

    clean up

--- src/main.c
+++ src/main.c
@@ -27,47 +27,10 @@
 
 #include "utils.h"
 
-static const char char_table[16] = {
-  /*0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  A,  B,  C,  D,  E,  F */
-    48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 65, 66, 67, 68, 69, 70};
-
-void buffer_fill(char * _buf);
-
 int main(void)
 {
-    DDRA = 0xFF;
-    PORTA = 0;
-
-    DDRC = 0;
-    PORTC = 0;
-
-    char str[] = "1 2 3 4 5 6 7 8 \r\n";
-    uart0_init(UART_BAUD_SELECT(9600, F_CPU));
-    sei();
-    uart0_puts(str);
-
     while(1){
-        char buf[19];
-        buf[16]='\n';
-        buf[17]='\r';
 
-        buffer_fill(buf);
-        uart0_puts(buf);
-//        uart0_putc(PORTC);
-        _delay_ms(1000);
-    }
-
-
-}
-
-void buffer_fill(char _buf[]){
-    for (uint8_t ind, i = 0; i < 8; ++i) {
-        PORTA = 1 << i;
-        ind = (PINC & 0xf0) >> 8;
-        _buf[i*2] = char_table[ind];
-        ind = PINC & 0x0f;
-        _buf[i*2+1] = char_table[ind];
-        PORTA = 0;
     }
 }
 
